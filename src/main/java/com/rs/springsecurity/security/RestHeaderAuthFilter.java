6103389ad976fef4e1dc040696e022a2589bf42a
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-22 10:36:16 -0500

    Added RestHeaderAuthFilter in Security package

---
+++ src/main/java/com/rs/springsecurity/security/RestHeaderAuthFilter.java
@@ -0,0 +1,56 @@
+package com.rs.springsecurity.security;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * created by rs 3/22/2022.
+ */
+@Slf4j
+public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter {
+
+    public RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        String userName = getUserName(request);
+        String password = getPassword(request);
+
+        if (userName == null){
+            userName = "";
+        }
+
+        if (password == null){
+            password = "";
+        }
+
+        log.debug("Authenticating User: " + userName);
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userName,password);
+        return this.getAuthenticationManager().authenticate(token);
+
+    }
+
+    private String getUserName(HttpServletRequest request){
+        return request.getHeader("Api-Key");
+    }
+    
+    private String getPassword(HttpServletRequest request){
+        return request.getHeader("Api-Secret");
+    }
+
+}
