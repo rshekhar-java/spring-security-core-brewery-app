576728604099ce106aedb717bf110f9ed3476294
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-22 09:57:09 -0500

    Added deleteBeer test in Rest Controller

--- src/test/java/com/rs/springsecurity/web/controllers/api/BeerRestControlllerTest.java
+++ src/test/java/com/rs/springsecurity/web/controllers/api/BeerRestControlllerTest.java
@@ -4,6 +4,7 @@ import com.rs.springsecurity.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +15,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest
 public class BeerRestControlllerTest extends BaseIT {
+
+/*    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .header("Api-Key", "spring").header("Api-Secret", "spring"))
+                .andExpect(status().isOk());
+    }*/
+
     @Test
     void findBeers() throws Exception{
         mockMvc.perform(get("/api/v1/beer/"))
