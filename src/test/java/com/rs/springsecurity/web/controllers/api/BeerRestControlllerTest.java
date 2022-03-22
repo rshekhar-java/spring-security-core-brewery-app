8b13f309a765052f3974c5ea76df58215276b545
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-22 11:57:24 -0500

    Updated RestHeaderAuthFilter and added delete beer test Scenario in BeerRestControlllerTest

--- src/test/java/com/rs/springsecurity/web/controllers/api/BeerRestControlllerTest.java
+++ src/test/java/com/rs/springsecurity/web/controllers/api/BeerRestControlllerTest.java
@@ -4,6 +4,7 @@ import com.rs.springsecurity.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +23,20 @@ public class BeerRestControlllerTest extends BaseIT {
                         .header("Api-Key", "spring").header("Api-Secret", "spring"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void deleteBeerHttpBasic() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .with(httpBasic("spring", "spring")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void deleteBeerNoAuth() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isUnauthorized());
+    }
+
 
     @Test
     void findBeers() throws Exception{
