e55bf20731bc44c1fceb9b5845f7cfc614d68c0b
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-23 18:53:58 -0500

    Added JpaUserDetailsService

---
+++ src/main/java/com/rs/springsecurity/security/JpaUserDetailsService.java
@@ -0,0 +1,49 @@
+package com.rs.springsecurity.security;
+
+import com.rs.springsecurity.domain.security.Authority;
+import com.rs.springsecurity.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.rs.springsecurity.domain.security.Users;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * created by rs 3/23/2022.
+ */
+@RequiredArgsConstructor
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepository.findByUsername(username).orElseThrow(() -> {
+            return new UsernameNotFoundException("User name: " + username + " not found");
+        });
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.getEnabled(), user.getAccountNonExpired(), user.getCredentialsNonExpired(),
+                user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
+    }
+
+    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
+        if (authorities != null && authorities.size() > 0){
+            return authorities.stream()
+                    .map(Authority::getRole)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        } else {
+            return new HashSet<>();
+        }
+    }
+}
